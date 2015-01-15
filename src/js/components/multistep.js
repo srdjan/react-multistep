cddf88160b6c3147aa45baf90f52170a6c383211
srdjan <srdjan@timok.com>
2015-01-15 16:55:54 -0500

    v0.1

--- src/js/components/multistep.js
+++ src/js/components/multistep.js
@@ -1,69 +1,74 @@
 var React = require('react/addons')
 var expect = require('expect.js')
 
-function navStates(curr, length) {
-  var navstates = []
+function navStates(indx, length) {
+  var navstates = {current: indx, styles: []}
   for (var i=0; i<length; i++) {
-    if(i < curr) {
-      navstates.push('done')
+    if(i < indx) {
+      navstates.styles.push('done')
     }
-    else if(i === curr) {
-      navstates.push('doing')
+    else if(i === indx) {
+      navstates.styles.push('doing')
     }
     else {
-      navstates.push('todo')
+      navstates.styles.push('todo')
     }
   }
   return navstates
 }
-//::test-start
-expect(navStates(0, 4)).to.eql(['doing','todo','todo','todo'])
-expect(navStates(1, 4)).to.eql(['done','doing','todo','todo'])
-expect(navStates(2, 4)).to.eql(['done','done','doing','todo'])
-expect(navStates(3, 4)).to.eql(['done','done','done','doing'])
-expect(navStates(4, 4)).to.eql(['done','done','done','done'])
-//::test-stop
+expect(navStates(0, 4).styles).to.eql(['doing','todo','todo','todo'])
+expect(navStates(1, 4).styles).to.eql(['done','doing','todo','todo'])
+expect(navStates(2, 4).styles).to.eql(['done','done','doing','todo'])
+expect(navStates(3, 4).styles).to.eql(['done','done','done','doing'])
+expect(navStates(4, 4).styles).to.eql(['done','done','done','done'])
 
 var Multistep = React.createClass({
   getInitialState() {
     return {
-        current: 0,
-        navState: navStates(0, this.props.steps.length+1)
+        compState: 0,
+        navState: navStates(0, this.props.steps.length)
       }
   },
 
-  nextNav(curr) {
-    this.setState({current: curr})
-    this.setState({navState: navStates(curr, this.props.steps.length)})
-    console.log(this.state.navState)
+  nextNav(nextComp, nextNav) {
+    if(nextComp < this.props.steps.length) {
+      this.setState({compState: nextComp})
+    }
+    if(nextComp <= this.props.steps.length) {
+      this.setState({navState: navStates(nextNav, this.props.steps.length)})
+    }
   },
 
-  handleOnNav(evt) {
-    this.nextNav(evt.target.value)
+  handleOnClick(evt) {
+    if(this.state.compState === this.props.steps.length-1 &&
+       this.state.navState.current === this.props.steps.length-1) {
+      this.nextNav(evt.target.value, evt.target.value+1)
+    }
+    else {
+      this.nextNav(evt.target.value, evt.target.value)
+    }
   },
 
   handleKeyDown(evt) {
-    if(evt.which === 13 && this.state.current <= navStates.length) {
-      this.nextNav(this.state.current + 1)
+    if(evt.which === 13) {
+      this.nextNav(this.state.navState.current + 1, this.state.navState.current + 1)
     }
   },
 
   render() {
     return (
       <div className="container" onKeyDown={this.handleKeyDown}>
-        <ol className="progtrckr">
-          {this.props.steps.map((s, i) =>
-            <li value={i} key={i}
-              className={"progtrckr-" + this.state.navState[i]}
-              onClick={this.handleOnNav}>
+        <ol className="progtrckr">{
+          this.props.steps.map((s, i) =>
+          <li value={i} key={i}
+                        className={"progtrckr-" + this.state.navState.styles[i]}
+                        onClick={this.handleOnClick}>
             <em>{i+1}</em>
             <span>{this.props.steps[i].name}</span>
           </li>
           )}
         </ol>
-        <div className="container">
-          {this.props.steps[this.state.current].component}
-        </div>
+        {this.props.steps[this.state.compState].component}
       </div>
     )}
 })
