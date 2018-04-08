160b10a1d2142818424e373469453bd52a97e6dc
Srdjan Strbanovic <srdjan@timok.com>
2018-04-08 10:29:17 -0400

    using Property Initializers

--- src/index.js
+++ src/index.js
@@ -1,129 +1,128 @@
-import React from 'react';
+import React from 'react'
+
+const getNavStates = (indx, length) => {
+  let styles = []
+  for (let i = 0; i < length; i++) {
+    if (i < indx) {
+      styles.push('done')
+    } else if (i === indx) {
+      styles.push('doing')
+    } else {
+      styles.push('todo')
+    }
+  }
+  return { current: indx, styles: styles }
+}
+
+const checkNavState = (currentStep, stepsLength) => {
+  if (currentStep > 0 && currentStep < stepsLength - 1) {
+    return {
+      showPreviousBtn: true,
+      showNextBtn: true
+    }
+  } else if (currentStep === 0) {
+    return {
+      showPreviousBtn: false,
+      showNextBtn: true
+    }
+  } else {
+    return {
+      showPreviousBtn: true,
+      showNextBtn: false
+    }
+  }
+}
 
 export default class MultiStep extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPreviousBtn: false,
-      showNextBtn: true,
-      compState: 0,
-      navState: this.getNavStates(0, this.props.steps.length)
-    };
-    this.hidden = {
-      display: 'none'
-    };
-    this.handleOnClick = this.handleOnClick.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
+  state = {
+    showPreviousBtn: false,
+    showNextBtn: true,
+    compState: 0,
+    navState: getNavStates(0, this.props.steps.length)
   }
 
-  getNavStates(indx, length) {
-    let styles = [];
-    for (let i=0; i<length; i++) {
-      if(i < indx) {
-        styles.push('done')
-      }
-      else if(i === indx) {
-        styles.push('doing')
-      }
-      else {
-        styles.push('todo')
-      }
-    }
-    return { current: indx, styles: styles }
-  }
-
-  checkNavState(currentStep){
-    if(currentStep > 0 && currentStep < this.props.steps.length - 1){
-      this.setState({
-        showPreviousBtn: true,
-        showNextBtn: true
-      })
-    }
-    else if(currentStep === 0) {
-      this.setState({
-        showPreviousBtn: false,
-        showNextBtn: true
-      })
-    }
-    else {
-      this.setState({
-        showPreviousBtn: true,
-        showNextBtn: false
-      })
-    }
-  }
-
-  setNavState(next) {
-    this.setState({navState: this.getNavStates(next, this.props.steps.length)})
+  setNavState = next => {
+    this.setState({
+      navState: getNavStates(next, this.props.steps.length)
+    })
     if (next < this.props.steps.length) {
-      this.setState({compState: next})
+      this.setState({ compState: next })
     }
-    this.checkNavState(next);
+    this.setState(checkNavState(next, this.props.steps.length))
   }
 
-  handleKeyDown(evt) {
+  handleKeyDown = evt => {
     if (evt.which === 13) {
       this.next()
     }
   }
 
-  handleOnClick(evt) {
-    if (evt.currentTarget.value === (this.props.steps.length - 1) &&
-      this.state.compState === (this.props.steps.length - 1)) {
+  handleOnClick = evt => {
+    if (
+      evt.currentTarget.value === this.props.steps.length - 1 &&
+      this.state.compState === this.props.steps.length - 1
+    ) {
       this.setNavState(this.props.steps.length)
-    }
-    else {
+    } else {
       this.setNavState(evt.currentTarget.value)
     }
   }
 
-  next() {
+  next = () => {
     this.setNavState(this.state.compState + 1)
   }
 
-  previous() {
+  previous = () => {
     if (this.state.compState > 0) {
       this.setNavState(this.state.compState - 1)
     }
   }
 
-  getClassName(className, i){
-    return className + "-" + this.state.navState.styles[i];
+  getClassName = (className, i) => {
+    return className + '-' + this.state.navState.styles[i]
   }
 
-  renderSteps() {
-    return this.props.steps.map((s, i)=> (
-      <li className={this.getClassName("progtrckr", i)} onClick={this.handleOnClick} key={i} value={i}>
-        <em>{i+1}</em>
+  renderSteps = () => {
+    return this.props.steps.map((s, i) => (
+      <li
+        className={this.getClassName('progtrckr', i)}
+        onClick={this.handleOnClick}
+        key={i}
+        value={i}
+      >
+        <em>{i + 1}</em>
         <span>{this.props.steps[i].name}</span>
       </li>
-    ));
+    ))
   }
 
-  render() {
-
+  render () {
     return (
-      <div className="container" onKeyDown={this.handleKeyDown}>
-        <ol className="progtrckr">
+      <div className='container' onKeyDown={this.handleKeyDown}>
+        <ol className='progtrckr'>
           {this.renderSteps()}
         </ol>
         {this.props.steps[this.state.compState].component}
-        <div style={this.props.showNavigation ? {} : this.hidden}>
-          <button style={this.state.showPreviousBtn ? {} : this.hidden}
-                  className="multistep__btn--prev"
-                  onClick={this.previous}>Previous</button>
+        <div style={this.props.showNavigation ? {} : { display: 'none' }}>
+          <button
+            style={this.state.showPreviousBtn ? {} : { display: 'none' }}
+            onClick={this.previous}
+          >
+            Previous
+          </button>
 
-          <button style={this.state.showNextBtn ? {} : this.hidden}
-                  className="multistep__btn--next"
-                  onClick={this.next}>Next</button>
+          <button
+            style={this.state.showNextBtn ? {} : { display: 'none' }}
+            onClick={this.next}
+          >
+            Next
+          </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
 MultiStep.defaultProps = {
   showNavigation: true
-};
+}
