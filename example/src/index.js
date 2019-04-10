e8ade533b0032457f25fef0248e8838728b8da62
Srdjan Strbanovic <srdjan@timok.com>
2019-04-10 07:35:31 -0400

    fic for exporting object, not a component

--- example/src/index.js
+++ example/src/index.js
@@ -1,25 +1,22 @@
 'use strict'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MultiStep from '../../dist/lib'
+import MultiStep from 'react-multistep'
 import { steps } from './steps'
 
-console.log(MultiStep)
+const App = () => (
+  <div className='container'>
+    <div>
+      <MultiStep steps={steps} />
+    </div>
+    <div className='container app-footer'>
+      <h6>Press 'Enter' or click on progress bar for next step.</h6>
+      Code is on{' '}
+      <a href='https://github.com/Srdjan/react-multistep' target='_blank'>
+        github
+      </a>
+    </div>
+  </div>
+)
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div>
-          <MultiStep steps={steps}/>
-        </div>
-        <div className="container app-footer">
-          <h6>Press 'Enter' or click on progress bar for next step.</h6>
-           Code is on <a href="https://github.com/Srdjan/react-multistep" target="_blank">github</a>
-        </div>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById('app'))
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-  entry: [
-    './index.js'
-  ],
+  entry: './index.js',
+  output: {
+    library: 'react-multistep',
+    libraryTarget: 'umd',
+    filename: 'lib.js',
+    globalObject: 'this'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react']
         }
       }
     ]
   }
-};
+}
