1c4cab169e3a2f3ce96e50b055409420ae8c4cd9
Srdjan Strbanovic <srdjan@timok.com>
2020-05-02 20:59:33 -0400

    bumped esbuild

--- index.js
+++ index.js
@@ -1,138 +1,16 @@
-let __defineProperty = Object.defineProperty;
-let __hasOwnProperty = Object.hasOwnProperty;
-let __modules = {};
-let __require = (id) => {
-  let module = __modules[id];
-  if (!module) {
-    module = __modules[id] = {
-      exports: {}
-    };
-    __commonjs[id](module.exports, module);
-  }
-  return module.exports;
-};
-let __toModule = (module) => {
-  if (module && module.__esModule) {
-    return module;
-  }
-  let result = {};
-  for (let key in module) {
-    if (__hasOwnProperty.call(module, key)) {
-      result[key] = module[key];
-    }
-  }
-  result.default = module;
-  return result;
-};
-let __import = (id) => {
-  return __toModule(__require(id));
-};
-let __export = (target, all) => {
-  __defineProperty(target, "__esModule", {
-    value: true
-  });
-  for (let name in all) {
-    __defineProperty(target, name, {
-      get: all[name],
-      enumerable: true
-    });
-  }
-};
-let __commonjs;
-__commonjs = {
-  0(exports) {
-    // node_modules/goober/dist/goober.js
-    var r = {
-      data: ""
-    }, t = function(t2) {
-      try {
-        var e2 = t2 ? t2.querySelector("#_goober") : self._goober;
-        return e2 || ((e2 = (t2 || document.head).appendChild(document.createElement("style"))).innerHTML = " ", e2.id = "_goober"), e2.firstChild;
-      } catch (r2) {
-      }
-      return r;
-    }, e = /(?:([a-z0-9-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(})/gi, a = /\/\*.*?\*\/|\s{2,}|\n/gm, n = function(r2, t2, e2) {
-      var a2 = "", o2 = "", s2 = "";
-      for (var c2 in r2) {
-        var i2 = r2[c2];
-        if ("object" == typeof i2) {
-          var u2 = t2 + " " + c2;
-          /&/g.test(c2) && (u2 = c2.replace(/&/g, t2)), "@" == c2[0] && (u2 = t2, "f" == c2[1] && (u2 = c2)), /@k/.test(c2) ? o2 += c2 + "{" + n(i2, "", "") + "}" : o2 += n(i2, u2, u2 == t2 ? c2 : e2 || "");
-        } else
-          /^@i/.test(c2) ? s2 = c2 + " " + i2 + ";" : a2 += c2.replace(/[A-Z]/g, "-$&").toLowerCase() + ":" + i2 + ";";
-      }
-      if (a2.charCodeAt(0)) {
-        var f2 = t2 + "{" + a2 + "}";
-        return e2 ? o2 + e2 + "{" + f2 + "}" : s2 + f2 + o2;
-      }
-      return s2 + o2;
-    }, o = {}, s = function(r2, t2, s2, c2) {
-      var i2 = JSON.stringify(r2), u2 = o[i2] || (o[i2] = ".go" + i2.split("").reduce(function(r3, t3) {
-        return 101 * r3 + t3.charCodeAt(0) >>> 0;
-      }, 11));
-      return function(r3, t3, e2) {
-        t3.data.indexOf(r3) < 0 && (t3.data = e2 ? r3 + t3.data : t3.data + r3);
-      }(o[u2] || (o[u2] = n(r2[0] ? function(r3) {
-        for (var t3, n2 = [{}]; t3 = e.exec(r3.replace(a, "")); )
-          t3[4] && n2.shift(), t3[3] ? n2.unshift(n2[0][t3[3]] = n2[0][t3[3]] || {}) : t3[4] || (n2[0][t3[1]] = t3[2]);
-        return n2[0];
-      }(r2) : r2, s2 ? "" : u2)), t2, c2), u2.slice(1);
-    }, c = function(r2, t2, e2) {
-      return r2.reduce(function(r3, a2, n2) {
-        var o2 = t2[n2];
-        if (o2 && o2.call) {
-          var s2 = o2(e2), c2 = s2 && s2.props && s2.props.className || /^go/.test(s2) && s2;
-          o2 = c2 ? "." + c2 : s2 && s2.props ? "" : s2;
-        }
-        return r3 + a2 + (null == o2 ? "" : o2);
-      }, "");
-    };
-    function i(r2) {
-      var e2 = this || {}, a2 = r2.call ? r2(e2.p) : r2;
-      return s(a2.map ? c(a2, [].slice.call(arguments, 1), e2.p) : a2, t(e2.target), e2.g, e2.o);
-    }
-    var u, f = i.bind({
-      g: 1
-    });
-    exports.styled = function(r2) {
-      var t2 = this || {};
-      return function() {
-        var e2 = arguments;
-        return function(a2) {
-          var n2 = t2.p = Object.assign({}, a2), o2 = n2.className;
-          return t2.o = /\s*go[0-9]+/g.test(o2), n2.className = i.apply(t2, e2) + (o2 ? " " + o2 : ""), u(r2, n2);
-        };
-      };
-    }, exports.setPragma = function(r2) {
-      return u = r2;
-    }, exports.extractCss = function(r2) {
-      var e2 = t(r2), a2 = e2.data;
-      return e2.data = "", a2;
-    }, exports.css = i, exports.glob = f;
-  },
-
-  1(exports) {
-    // react-multistep.js
-    __export(exports, {
-      default: () => MultiStep
-    });
-    const react = __toModule(require("react"));
-    const goober = __import(0 /* goober */);
-    goober.setPragma(react.default.createElement);
-    const Ol = goober.styled("ol")`
+let e$=Object.defineProperty,f$=Object.hasOwnProperty,h$={},j$,k$=a$=>{let b$=h$[a$];return b$||(b$=h$[a$]={exports:{}},j$[a$](b$.exports,b$)),b$.exports},l$=a$=>{if(a$&&a$.__esModule)return a$;let b$={};for(let c$ in a$)f$.call(a$,c$)&&(b$[c$]=a$[c$]);return b$.default=a$,b$},o$=a$=>l$(k$(a$)),p$=(a$,b$)=>{e$(a$,"__esModule",{value:!0});for(let c$ in b$)e$(a$,c$,{get:b$[c$],enumerable:!0})};j$={0(j){var q={data:""},n=function(d){try{var c=d?d.querySelector("#_goober"):self._goober;return c||((c=(d||document.head).appendChild(document.createElement("style"))).innerHTML=" ",c.id="_goober"),c.firstChild}catch(f){}return q},r=/(?:([a-z0-9-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(})/gi,s=/\/\*.*?\*\/|\s{2,}|\n/gm,l=function(d,c,f){var i="",g="",h="";for(var b in d){var a=d[b];if("object"==typeof a){var e=c+" "+b;/&/g.test(b)&&(e=b.replace(/&/g,c)),"@"==b[0]&&(e=c,"f"==b[1]&&(e=b)),/@k/.test(b)?g+=b+"{"+l(a,"","")+"}":g+=l(a,e,e==c?b:f||"")}else/^@i/.test(b)?h=b+" "+a+";":i+=b.replace(/[A-Z]/g,"-$&").toLowerCase()+":"+a+";"}if(i.charCodeAt(0)){var p=c+"{"+i+"}";return f?g+f+"{"+p+"}":h+p+g}return h+g},k={},t=function(d,c,f,i){var g=JSON.stringify(d),h=k[g]||(k[g]=".go"+g.split("").reduce(function(b,a){return 101*b+a.charCodeAt(0)>>>0},11));return function(b,a,e){a.data.indexOf(b)<0&&(a.data=e?b+a.data:a.data+b)}(k[h]||(k[h]=l(d[0]?function(b){for(var a,e=[{}];a=r.exec(b.replace(s,""));)a[4]&&e.shift(),a[3]?e.unshift(e[0][a[3]]=e[0][a[3]]||{}):a[4]||(e[0][a[1]]=a[2]);return e[0]}(d):d,f?"":h)),c,i),h.slice(1)},u=function(d,c,f){return d.reduce(function(i,g,h){var b=c[h];if(b&&b.call){var a=b(f),e=a&&a.props&&a.props.className||/^go/.test(a)&&a;b=e?"."+e:a&&a.props?"":a}return i+g+(null==b?"":b)},"")};function m(d){var c=this||{},f=d.call?d(c.p):d;return t(f.map?u(f,[].slice.call(arguments,1),c.p):f,n(c.target),c.g,c.o)}var o,v=m.bind({g:1});j.styled=function(d){var c=this||{};return function(){var f=arguments;return function(i){var g=c.p=Object.assign({},i),h=g.className;return c.o=/\s*go[0-9]+/g.test(h),g.className=m.apply(c,f)+(h?" "+h:""),o(d,g)}}},j.setPragma=function(d){return o=d},j.extractCss=function(d){var c=n(d),f=c.data;return c.data="",f},j.css=m,j.glob=v},1(o){p$(o,{default:()=>F});const D=l$(require("react")),E=o$(0);E.setPragma(D.default.createElement);const s=E.styled("ol")`
   margin: 0;
   padding-bottom: 2.2rem;
   list-style-type: none;
-`;
-    const LiClass = (props) => goober.css`
+`,t=a=>E.css`
   display: inline-block;
   text-align: center;
   line-height: 4.5rem;
   padding: 0 0.7rem;
   cursor: pointer;
 
-  color: ${props.state === "todo" ? "silver" : "black"};
-  border-bottom: 4px solid ${props.state === "todo" ? "silver" : "#33C3F0"};
+  color: ${a.state==="todo"?"silver":"black"};
+  border-bottom: 4px solid ${a.state==="todo"?"silver":"#33C3F0"};
 
   &:before {
     position: relative;
@@ -140,12 +18,12 @@ __commonjs = {
     float: left;
     left: 50%;
 
-    ${props.state === "todo" ? 'content: "Ο";' : props.state === "doing" ? 'content: "•";' : 'content: "✓";'}
-    color: ${props.state === "todo" ? "silver" : "white"};
-    background-color: ${props.state === "todo" ? "white" : "#33C3F0"};  
+    ${a.state==="todo"?'content: "Ο";':a.state==="doing"?'content: "•";':'content: "✓";'}
+    color: ${a.state==="todo"?"silver":"white"};
+    background-color: ${a.state==="todo"?"white":"#33C3F0"};  
     width: 1.2em;
-    line-height: ${props.state === "todo" ? "1.2em" : "1.4em"};
-    border-radius: ${props.state === "todo" ? "0" : "1.2em"};
+    line-height: ${a.state==="todo"?"1.2em":"1.4em"};
+    border-radius: ${a.state==="todo"?"0":"1.2em"};
   }
   &:hover,
   &::before {
@@ -157,83 +35,4 @@ __commonjs = {
   span {
     padding: 0 1.5rem;
   }
-`;
-    const getNavStyles = (indx, length) => {
-      let styles = [];
-      for (let i = 0; i < length; i++) {
-        if (i < indx) {
-          styles.push("done");
-        } else if (i === indx) {
-          styles.push("doing");
-        } else {
-          styles.push("todo");
-        }
-      }
-      return styles;
-    };
-    const getButtonsState = (indx, length) => {
-      if (indx > 0 && indx < length - 1) {
-        return {
-          showPreviousBtn: true,
-          showNextBtn: true
-        };
-      } else if (indx === 0) {
-        return {
-          showPreviousBtn: false,
-          showNextBtn: true
-        };
-      } else {
-        return {
-          showPreviousBtn: true,
-          showNextBtn: false
-        };
-      }
-    };
-    function MultiStep(props) {
-      let showNavigation = true;
-      if (props.showNavigation && props.showNavigation)
-        showNavigation = props.showNavigation;
-      const [stylesState, setStyles] = react.useState(getNavStyles(0, props.steps.length));
-      const [compState, setComp] = react.useState(0);
-      const [buttonsState, setButtons] = react.useState(getButtonsState(0, props.steps.length));
-      const setStepState = (indx) => {
-        setStyles(getNavStyles(indx, props.steps.length));
-        setComp(indx < props.steps.length ? indx : compState);
-        setButtons(getButtonsState(indx, props.steps.length));
-      };
-      const next = () => setStepState(compState + 1);
-      const previous = () => setStepState(compState > 0 ? compState - 1 : compState);
-      const handleKeyDown = (evt) => evt.which === 13 ? next(props.steps.length) : {};
-      const handleOnClick = (evt) => {
-        if (evt.currentTarget.value === props.steps.length - 1 && compState === props.steps.length - 1) {
-          setStepState(props.steps.length);
-        } else {
-          setStepState(evt.currentTarget.value);
-        }
-      };
-      const renderSteps = () => props.steps.map((s, i) => react.default.createElement("li", {
-        className: LiClass({
-          state: stylesState[i]
-        }),
-        onClick: handleOnClick,
-        key: i,
-        value: i
-      }, react.default.createElement("span", null, i + 1)));
-      const renderNav = (show) => show && react.default.createElement("div", null, react.default.createElement("button", {
-        style: buttonsState.showPreviousBtn ? {} : {
-          display: "none"
-        },
-        onClick: previous
-      }, "Prev"), react.default.createElement("button", {
-        style: buttonsState.showNextBtn ? {} : {
-          display: "none"
-        },
-        onClick: next
-      }, "Next"));
-      return react.default.createElement("div", {
-        onKeyDown: handleKeyDown
-      }, react.default.createElement(Ol, null, renderSteps()), react.default.createElement("div", null, props.steps[compState].component), react.default.createElement("div", null, renderNav(showNavigation)));
-    }
-  }
-};
-module.exports = __require(1);
+`,k=(a,e)=>{let f=[];for(let g=0;g<e;g++)g<a?f.push("done"):g===a?f.push("doing"):f.push("todo");return f},l=(a,e)=>a>0&&a<e-1?{showPreviousBtn:!0,showNextBtn:!0}:a===0?{showPreviousBtn:!1,showNextBtn:!0}:{showPreviousBtn:!0,showNextBtn:!1};function F(a){let e=!0;a.showNavigation&&a.showNavigation&&(e=a.showNavigation);const[f,g]=D.useState(k(0,a.steps.length)),[d,u]=D.useState(0),[m,v]=D.useState(l(0,a.steps.length)),h=b=>{g(k(b,a.steps.length)),u(b<a.steps.length?b:d),v(l(b,a.steps.length))},n=()=>h(d+1),w=()=>h(d>0?d-1:d),x=b=>b.which===13?n(a.steps.length):{},y=b=>{b.currentTarget.value===a.steps.length-1&&d===a.steps.length-1?h(a.steps.length):h(b.currentTarget.value)},z=()=>a.steps.map((b,i)=>D.default.createElement("li",{className:t({state:f[i]}),onClick:y,key:i,value:i},D.default.createElement("span",null,i+1))),A=b=>b&&D.default.createElement("div",null,D.default.createElement("button",{style:m.showPreviousBtn?{}:{display:"none"},onClick:w},"Prev"),D.default.createElement("button",{style:m.showNextBtn?{}:{display:"none"},onClick:n},"Next"));return D.default.createElement("div",{onKeyDown:x},D.default.createElement(s,null,z()),D.default.createElement("div",null,a.steps[d].component),D.default.createElement("div",null,A(e)))}}};module.exports=k$(1);
