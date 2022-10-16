c3c28f6a1fdd7a7e86596b99311e20d7a3ce9ce8
ofirelarat <96ofir11@gmail.com>
2022-10-16 19:46:18 +0300

    added test template

--- src/stories/MultiStep.stories.jsx
+++ src/stories/MultiStep.stories.jsx
@@ -13,7 +13,6 @@ export default {
 
 const Template = (args) => <MultiStep {...args} />;
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const defaultTemplate = Template.bind({});
 defaultTemplate.args = {
   steps: [
@@ -22,5 +21,17 @@ defaultTemplate.args = {
     { component: <div>3</div> },
     { component: <div>4</div> }
   ],
-  activeStep:0
+  activeStep: 0
+};
+
+
+export const withTitlesTemplate = Template.bind({});
+withTitlesTemplate.args = {
+  steps: [
+    { name: 'step one', component: <div>1</div> },
+    { name: 'step two', component: <div>2</div> },
+    { name: 'step three', component: <div>3</div> },
+    { name: 'step four', component: <div>4</div> }
+  ],
+  activeStep: 0
 };
