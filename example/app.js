4d91a543c55c152c34919b0913e62c1c59a0e7c1
ofirelarat <96ofir11@gmail.com>
2023-07-10 20:53:45 +0300

    fix ReactDOM.render no longer support React 18

--- example/app.js
+++ example/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import MultiStep from 'react-multistep'
 import StepOne from './stepOne'
 import StepTwo from './stepTwo'
@@ -24,4 +24,5 @@ const App = () => (
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+root.render(<App />)
