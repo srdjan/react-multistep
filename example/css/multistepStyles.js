2d66ac47d4c874e04743e756903d4ca3d1b56bc1
Srdjan <srdjan@timok.com>
2023-07-22 13:10:08 -0400

    fixed top nav style (new)

--- example/css/multistepStyles.js
+++ example/css/multistepStyles.js
@@ -8,7 +8,20 @@ export const topNavStyle = {
   margin: '0',
   paddingBottom: '2.2rem',
   listStyleType: 'none',
-  flexDirection: 'row'
+  flexDirection: 'row',
+  after: {
+    content:'\"\\\\00a0\\\\00a0\"'
+  },
+  before: {
+    color: '#0FA0CE',
+    position: 'relative',
+    float: 'left',
+    left: '50%',
+    width: '1.3em',
+    lineHeight: '1.4em',
+    borderRadius: '50%',
+    bottom: '-3.99rem'
+  }
 }
 
 export const topNavStepStyle = {
@@ -19,23 +32,35 @@ export const topNavStepStyle = {
   borderBottom: '1px solid silver'
 }
 export const Todo = {
-  color: 'silver'
+  color: 'silver',
+  before: {
+    content: '\"\\u039F\"',
+    backgroundColor: 'silver'
+  }
 }
 export const Doing = {
-  color: 'blue'
+  color: '#1EAEDB',
+  before: {
+    content: '\"\\u2022\"',
+    backgroundColor: '#33C3F0'
+  }
 }
 export const Done = {
-  color: 'green'
+  color: 'silver',
+  before: {
+    content: '\"\\u2713\"',
+    backgroundColor: '#33C3F0'
+  }
 }
 
 export const prevButton = {
   title: 'Back', 
-  style:{ color: 'white', marginRight: '1rem', marginTop: '1rem' },
-  disabledStyle:{ color: 'black', backgroundColor: 'gray', marginRight: '1rem', marginTop: '1rem' }
+  style:{ color: '#23addb', backgroundColor: '#f1f1f1', marginRight: '1rem', marginTop: '1rem' },
+  disabledStyle:{ color: '#c0c0c0', backgroundColor: '#d1d1d1', marginRight: '1rem', marginTop: '1rem' }
 }
 
 export const nextButton = {
   title: 'Forward', 
-  style:{ color: 'white', marginTop: '1rem' },
-  disabledStyle:{ color: 'black', backgroundColor: 'gray', marginTop: '1rem' }
+  style:{ color: '#23addb', backgroundColor: '#f1f1f1', marginTop: '1rem' },
+  disabledStyle:{ color: '#c0c0c0', backgroundColor: '#d1d1d1', marginTop: '1rem' }
 }
