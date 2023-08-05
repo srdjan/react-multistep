9eef18c404741aeb7cbe2fa88e5c34a054a0f944
Srdjan <srdjan@timok.com>
2023-08-05 10:12:40 -0400

    - more cleanup

--- src/MultiStep.tsx
+++ src/MultiStep.tsx
@@ -34,12 +34,10 @@ const getBottomNavState = (activeStep: number, length: number, stepIsValid: bool
 
 export default function MultiStep(props: MultiStepProps) {
   let { children } = props
-  if (!children) {
-    throw TypeError("Error: Application has no children Components configured")
-  }
+  if (!children) throw TypeError("Error: Application has no children Components configured")
 
   const styles = typeof props.styles === 'undefined' ? BaseStyles as MultiStepStyles : props.styles
-  const [activeChild, setActiveChild] = useState(0)
+  const [activeChild, setActive] = useState(0)
   const [childIsValid, setChildIsValid] = useState(false)
   const [topNavState, setTopNavState] = useState(getTopNavState(activeChild, children.length))
   const [bottomNavState, setBottomNavState] = useState(getBottomNavState(activeChild, children.length, childIsValid))
@@ -49,38 +47,33 @@ export default function MultiStep(props: MultiStepProps) {
     setBottomNavState(getBottomNavState(activeChild, children.length, childIsValid))
   }, [activeChild, childIsValid])
 
-  const childStateChanged = (childState: ChildState) => {
-    console.debug(`childStateChanged - isValid: ${childState?.isValid}`)
-    setChildIsValid(() => childState.isValid)
-  }  
+  const childStateChanged = (childState: ChildState) => setChildIsValid(() => childState.isValid)  
   children = React.Children.map(children, child => React.cloneElement(child, { signalParent: childStateChanged }))
 
-  const handleBottomNavNext = () => setActiveChild(activeChild === children.length - 1 ? activeChild : activeChild + 1)
+  const handleNext = () => setActive(activeChild === children.length - 1 ? activeChild : activeChild + 1)
 
-  const handleBottomNavPrevious = () => setActiveChild(activeChild > 0 ? activeChild - 1 : activeChild)
+  const handlePrevious = () => setActive(activeChild > 0 ? activeChild - 1 : activeChild)
 
-  const handleOnClick = (i: number) => childIsValid ? setActiveChild(i) : console.log('Error: Invalid state')
+  const handleOnClick = (i: number) => childIsValid ? setActive(i) : console.log('Error: Invalid state')
 
   const renderTopNav = () =>
     <ol style={styles.topNav}>
       {children.map((c, i) =>
         <li style={styles.topNavStep} onClick={() => handleOnClick(i)} key={i}>
-          {
-            topNavState[i] === 'doing' ? <span style={styles.doing}>{c.props.title ?? i + 1}</span> :
-                                         <span style={styles.todo}>{c.props.title ?? i + 1}</span>
-          }
+          { topNavState[i] === 'doing' ? <span style={styles.doing}>{c.props.title ?? i + 1}</span> :
+                                         <span style={styles.todo}>{c.props.title ?? i + 1}</span> }
         </li>
       )}
     </ol>
 
   const renderBottomNav = () =>
     <>
-      <button onClick={handleBottomNavPrevious}
+      <button onClick={handlePrevious}
               style={styles.prevButton}
               disabled={bottomNavState.prevDisabled}>
         <span>&#60;</span>
       </button>
-      <button onClick={handleBottomNavNext}
+      <button onClick={handleNext}
               style={bottomNavState.hideLast ? { display: 'none' } : styles.nextButton}
               disabled={bottomNavState.nextDisabled}>
         <span>&#62;</span>
